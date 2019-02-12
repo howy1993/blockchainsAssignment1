b83c30599c070d875b1e5e31d96dd67b7d4850f0
Howy <Howy@Howys-MBP.local>
2019-02-12 13:38:51 -0500

    cleaned up structure

--- main.py
+++ main.py
@@ -11,19 +11,10 @@ from hashlib import sha256 as H
 # }
 class Transaction:
     def __init__(self, inputs, outputs, sig):
-        self.input = inputs
-        self.output = outputs
-        self.sig = sig
-        self.number = H(b'inputs')
-        self.number.update(b'outputs')
+        return 1
 
     def verifyNumberHash():
-        temp = H(b'inputs')
-        temp.update(b'outputs')
-        if temp != number:
-            return 0
-        else:
-            return 1
+        return 1
 
 # block
 # {"tx": <a single transaction>,
@@ -33,48 +24,32 @@ class Transaction:
 # }
 class Block:
     def __init__(self, tx:Transaction, prev):
-        self.tx = tx
-        self.prev = prev
+        return 1
 
 class Node:
     def __init__(genBlock:Block, txList):
-        self.root = Node("root", blockNumber=1, block=genBlock, height=1)
-        self.utxoPool = txList
-        self.currentMaxHeight = 1
+        return 1
 
     def verifyNotUsed(_tx:Transaction):
-        if root.search.find(node.block.tx.number == "") != None:
-            return 1
-        else:
-            return 0
+        return 1
 
     def verifyInputUTXO(_tx:Transaction):
-        
         return 1
 
     def verifyInputOutput(_tx:Transaction):
-        return True
+        return 1
 
     def verifyDoubleSpend(_tx:Transaction):
-        return True
+        return 1
 
     def verifySum(_tx:Transaction):
-        return True
-        #tx.inputs
+        return 1
 
     def verify(_tx:Transaction):
-        flag = _tx.verifyNumberHash
-        flag *= verifyNotUsed(_tx)
-        flag *= verifyInputUTXO(_tx)
-        flag *= verifyInputOutput(_tx)
-        flag *= verifyDoubleSpend(_tx)
-        flag *= verifySum(_tx)
-        return bool(flag)
-
+        return 1
 
     def maxHeight():
-        return True
-
+        return 1
 
     def mineBlock(Transaction):
         return 1
