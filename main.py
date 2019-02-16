7c6d14cbc8ce3b86ede4d1e53a70443ae1230974
mangorocoro <daniel01001000@gmail.com>
2019-02-16 16:28:38 -0500

    signature hexs

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import nacl.signing
 import queue
 from hashlib import sha256 as H
 
+
 # Serializes a list of JSON objects from a specific transaction
 # input(s): json object, term (input or output)
 # output(s): a serialization of the list of inputs or outputs
@@ -26,6 +27,7 @@ def serialize(tx, term):
 # input(s): a transaction JSON object
 # output(s): a serialization of the JSON elements into a number
 def generate_number(tx):
+    print("generating number of transaction!")
     serials = []
     # serialize each transaction (each input and output)
     for ele in ["input", "output"]:
@@ -34,7 +36,7 @@ def generate_number(tx):
 
     d = json.loads(tx)
     # add signature
-    serials.append(str(d["sig"]))
+    serials.append(d["sig"])
     joinedSerials = "".join(serials)
     encodedSerials = joinedSerials.encode('utf-8')
     # hash the serialized data
@@ -42,15 +44,16 @@ def generate_number(tx):
 
     return hashedSerials
 
+
 # Serializes transaction, previous hash, and nonce value
-# input(s): transaction
-# output(s):
+# input(s): transaction object, prev string, nonce integer
+# output(s): a string representation of a block
 def serialize_block(tx, prev, nonce):
 
     # serialize specifically for a transaction
     serials = []
     for t in ["number", "input", "output", "sig"]:
-        res = serialize(tx, t)
+        res = serialize(tx.jsonify(), t)
         serials.append(res)
     serials.append(prev)
     serials.append(str(nonce))
@@ -58,11 +61,11 @@ def serialize_block(tx, prev, nonce):
 
     return joinedSerials
 
+
 # Helper function for test code for serializing list
 # input(s): list, term
 # output(s): serialized list
 def serialize_list(l, term):
-
     s = []
     for ele in l:
         if term == "input":
@@ -91,14 +94,26 @@ class Output:
 class Input:
     def __init__(self, number, output):
         self.number = number
-        self.output:Output = output #each input holds 1 output
+        self.output:Output = output  #each input holds 1 output
+
 
 class Transaction:
     def __init__(self, inputs, outputs, sig):
         self.input = inputs
         self.output = outputs
+        # is a signature object with both: sig.message, sig.signature
         self.sig = sig
         self.number = 0
+
+    def serialize_self(self):
+        s = []
+        s.append(serialize(self.jsonify(), "input"))
+        s.append(serialize(self.jsonify(), "output"))
+        # convert signature (bytes) to hex format (str) for serialization
+        # can revert sig (str) back to bytes with bytes.fromhex(sig)
+        s.append(self.sig.signature.hex())
+        s.append(str(self.number))
+        return ''.join(s)
 
     def gen_number(self):
         j = self.jsonify()
@@ -106,15 +121,24 @@ class Transaction:
         self.number = number.hexdigest()
 
     def verify_number_hash(self):
-        temp = generate_number(self.jsonify())
+        print("verifying number hash!")
+        jsonT = self.jsonify()
+        print("jsonT = {}".format(jsonT))
+        temp = generate_number(jsonT)
+        print("temp = {}".format(temp))
+        print("self = {}".format(self.number))
         return (temp == self.number)
 
     def jsonify(self):
+        print("jsonifying!")
         jsonObj = {}
 
         inputList = []
+        print("self.input = {}".format(self.input))
+
         for i in self.input:
             inputsOutDict = {}
+            inputsOutDict["output"] = {}
             inputsOutDict["number"] = i.number
             inputsOutDict["output"]["value"] = i.output.value
             inputsOutDict["output"]["pubkey"] = str(i.output.pubkey)
@@ -128,16 +152,14 @@ class Transaction:
             outputDict["pubkey"] = str(o.pubkey)
             outputList.append(outputDict)
         jsonObj["output"] = outputList
-
-        jsonObj["sig"] = self.sig
+        jsonObj["sig"] = self.sig.signature.hex()
         jsonObj["number"] = self.number
 
         return json.dumps(jsonObj, indent=4)
 
 
     def show(self):
-        print("input: {0}\noutput: {1}\nsig: {2}\nnumber: {3}\n"
-                .format(self.input, self.output, self.sig, self.number))
+        print("input: {0}\noutput: {1}\nsig: {2}\nnumber: {3}\n".format(self.input, self.output, self.sig.signature, self.number))
 
 
 # block
@@ -177,10 +199,13 @@ class Node:
         self.current_max_height_tree_node = self.root
         self.q = queue.Queue()
 
-    # Checks that tx number is has not been used in any prevBlocks
-    def verify_not_used(local_tx:Transaction):
-        y = current_max_height_tree_node
+    # Checks that tx number has not been used in any prevBlocks
+    def verify_not_used(self, local_tx:Transaction, treenode:TreeNode):
+        print("==== verify not used ====")
+        y = treenode
         while(y != None):
+            print("local_tx.number = {}".format(local_tx.number))
+            print("y.block.tx.number = {}".format(y.block.tx.number))
             if local_tx.number == y.block.tx.number:
                 return 0
             else:
@@ -188,7 +213,7 @@ class Node:
         return 1
 
     # Output exists in named transaction - this function checks for/matches numbers and output to an earlier tx
-    def verify_tx_inputs(local_tx:Transaction):
+    def verify_tx_inputs(self, local_tx:Transaction):
         flag = 1
         flag2 = 1
         for x in local_tx.input:
@@ -205,20 +230,20 @@ class Node:
                     return max(flag - len(tx.input),0)
         return max(flag - len(local_tx.input), 0)
     # Checks public key for all inputs, checks signature
-    def verify_public_key_signatures(tx:Transaction):
+    def verify_public_key_signatures(self, tx:Transaction):
         #check same public key
         pubkey1 = tx.input(1).output.pubkey
         for x in tx.input:
             if x.output.pubkey != pubkey1:
                 return 0
         #serializes content and verifies signature to key
-        message = serialize(tx.input, "input")
-        message += serialize(tx.output, "output")
+        message = serialize_list(tx.input, "input")
+        message += serialize_list(tx.output, "output")
         verify_key = nacl.signing.VerifyKey(verify_key_hex, encoder=nacl.encoding.HexEncoder)
-        return verify(message, tx.sig, encoder=nacl.encoding.HexEncoder)
+        return verify_key.verify(message)
 
     # ? Not sure if this logic is right
-    def verify_double_spend(tx:Transaction):
+    def verify_double_spend(self, tx:Transaction):
         flag = 1
         flag2 = 1
         for x in tx.input:
@@ -235,20 +260,21 @@ class Node:
         return max(flag - len(tx.input),0)
 
     # Checks sum of inputs vs outputs
-    def verify_sum(tx:Transaction):
+    def verify_sum(self, tx:Transaction):
         input_sum = 0
         output_sum = 0
         for x in tx.input:
             input_sum += x.output.value
         for y in tx.output:
             output_sum += y.value
-        return (x == y)
+        return (input_sum == output_sum)
 
-    def verify(tx:Transaction):
-        flag = tx.verifyNumberHash
-        flag *= verify_not_used(tx)
-        flag *= verify_input_UTXO(tx)
-        flag *= verify_input_output(tx)
+    def verify(self, tx:Transaction, treenode:TreeNode):
+        print("verifying!")
+        flag = tx.verify_number_hash()
+        flag *= self.verify_not_used(tx, treenode)
+        flag *= verify_tx_inputs(tx)
+        flag *= verify_public_key_signatures(tx)
         flag *= verify_double_spend(tx)
         flag *= verify_sum(tx)
         return bool(flag)
@@ -258,18 +284,28 @@ class Node:
                 self.current_max_height_tree_node = new_block_tree_node
 
     def mine_block(tx:Transaction, prev:Block):
-        proofow = 0x07FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF + hex(1)
+        proofow = 0x07FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF + 1
+        target = 0x07FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
         nonce = 0
-        while (proofow > 0x07FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF):
+        while (proofow > target):
             nonce += 1
-            block_message = serialize_block(tx.number, prev, nonce)
+            prev_serialized = serialize_block(prev)
+            prev_encoded = prev_serialized.encode('utf-8')
+            prevhash = H(prev_encoded).hexdigest()
+            block_message = serialize_pre_block(tx, prevhash, nonce)
             proofow = H(block_message.encode('utf-8'))
-        #Once verified, push nonce/pow/prev into a new block and send it out
+            proofow = proofow.digest()
+            target = b'target'
+
+        # Once verified, push nonce/pow/prev into a new block and send it out
         new_block = Block(tx, prev, nonce, proofow)
-        new_treenode = TreeNode(new_block, prev, prevBlock.height+1)
-        treenode_list.append(new_treenode)
-        update_longest_chain(new_block_tree_node)
-        sendBlock(new_block)
+        new_treenode = TreeNode(new_block, self.current_max_height_tree_node, self.current_max_height_tree_node.height+1)
+        self.treenode_list.append(new_treenode)
+        self.update_longest_chain(new_treenode)
+        self.send_block(new_block)
+
+
+
 
     def send_block(new_block):
         #for x in node_list:
@@ -281,141 +317,109 @@ class Node:
         #update_longest_chain(new_block)
         return 1
 
-    def mining(self):
-        print("does it come here?")
-        while(): #global variable
+    def mining(self, global_tx_pool):
+        print("starting mining...")
+        while(True): #global variable
             #sleep(random)
             print("looping")
-            if (not q.empty()):
-                new_block = q.get()
+            print("q size = {}".format(self.q.qsize()))
+            if (not self.q.empty()):
+                new_block = self.q.get()
 
-            if(not global_tx_pool.empty() and lock == 0):
-                lock = 1
-                print(len(lobal_tx_pool))
-                new_tx = q.get()
-                if verify(new_tx):
-                    mine_block(new_tx, current_max_height_tree_node.block)
-                send_block(new_block)
-                lock = 0
+            print("global_tx_pool = {}".format(global_tx_pool))
+            if(len(global_tx_pool) != 0):
+                new_tx = global_tx_pool[0]
+                del global_tx_pool[0]
+                print("new_tx = {}".format(new_tx))
+                if self.verify(new_tx, self.current_max_height_tree_node) == True:
+                    print("verified")
+                    self.mine_block(new_tx, current_max_height_tree_node.block)
 
             if(global_tx_pool.empty()):
                 return
 
+
 def main():
-    jObj = json.dumps([
-        {
-            "number": "fc12f4bc8657dd0139a978cd1bd64b94e5b780dc61f292830626ead7f830c221",
-            "input": [
-                {
-                    "number": "1234",
-                    "output": {
-                        "value": 123,
-                        "pubkey": "asdf"
-                    }
-                },
-                {
-                    "number": "5678",
-                    "output": {
-                        "value": 456,
-                        "pubkey": "fdsa"
-                    }
-                }
-            ],
-            "output": [
-                {
-                    "value": 40.87722055316936,
-                    "pubkey": "f55fcd64b06bae4f5f13ef7c33b0d71c8c2a9a0b446bdd9158e88fb1ec131b2f"
+    ## Start of test.
+    signing_key = []
+    verify_key = []
+    verify_key_hex = []
 
-                 },
-                {
-                    "value": 62.70091885420593,
-                    "pubkey": "f31afc7d3b351ed6627a6fe73e59c56cc85142b6ce75c91a6cb48ada8b18df7f"
-                },
-                {
-                    "value": 61.567598039085006,
-                    "pubkey": "a4358fe73763794164b5cc88605da3a9c877e157f2eef135fa717d9ba6e2f403"
-                },
-                {
-                    "value": 32.036403371644184,
-                    "pubkey": "8ae86a3d0886ed284558f1f1877e56ce7a471bd00db16effc506f10911916bcf"
-                },
-                {
-                    "value": 74.85502582411641,
-                    "pubkey": "807cfae7317ed443aacf114ff26dd9c69bcdc757f6c742d8cc1a2667be4e7ca5"
-                }
-            ],
-            "sig": "ec17086bd4c0a566893a508454f6ed332c360de2d86834e5ffeddeec4ec24da907dc505d595692415a1ade7f7bd9d03cf2e01acbd3335f1eef3b1ecfb9548a06"
-        }])
+    # Generate 8 random pksk pairs
+    for i in range(0, 8):
+        signing_key_new = nacl.signing.SigningKey.generate()
+        verify_key_new = signing_key_new.verify_key
+        verify_key_hex_new = verify_key_new.encode(encoder=nacl.encoding.HexEncoder)
+        signing_key.append(signing_key_new)
+        verify_key.append(verify_key_new)
+        verify_key_hex.append(verify_key_hex_new)
+
+    output_list = []
+    # Generate contents for gen block. All 8 pksk pairs get 100 coins
+    for i in range(0, 8):
+        output_list.append(Output(100, verify_key_hex[i]))
+
+    empty_input_list = []
+
+    # generate arbitrary signature object (contains message and signature)
+    arbi_signing_key = nacl.signing.SigningKey.generate()
+    arbi_signed = arbi_signing_key.sign(b"arbitrary signing key")
+    gen_transaction = Transaction(empty_input_list, output_list, arbi_signed)
+
+    j = gen_transaction.jsonify()
+
+    # generate the number for the transaction
+    gen_transaction.gen_number()
+    gen_transaction_number = gen_transaction.number
+
+    #print("gen_transaction_number = {}".format(gen_transaction_number))
+    
+    arbiPrev = H(b'arbitrary prev').hexdigest()
+    arbiNonce = H(b'arbitrary nonce').hexdigest()
+    arbiPow = H(b'arbitrary pow').hexdigest()
 
 
+    # Generate genesis block
+    gen_block = Block(gen_transaction, arbiPrev, arbiNonce, arbiPow)
+
+    # Initialize all nodes with genesis block
+    node_list = []
+    for i in range(0, 10):
+        node_list.append(Node(gen_block))
 
 
-##
-## Start of test.
-##
+    # populate each node's node_list with every other node
+    for i in range(0, 10):
+        node_list[i].node_list = node_list
 
-signing_key = []
-verify_key = []
-verify_key_hex = []
+    global_tx_pool = []
+    no_more_tx = 1
+    inputs_from_gen_tx = []
+    outputs_from_gen_tx = []
+    sig = []
 
-# Generate 8 random pksk pairs
-for i in range (0,8):
-    signing_key_new = nacl.signing.SigningKey.generate()
-    verify_key_new = signing_key_new.verify_key
-    verify_key_hex_new = verify_key_new.encode(encoder=nacl.encoding.HexEncoder)
-    signing_key.append(signing_key_new)
-    verify_key.append(verify_key_new)
-    verify_key_hex.append(verify_key_hex_new)
+    for i in range(0, 3):
+        new1 = []
+        new2 = []
+        new1.append(Input(gen_transaction_number, Output(100, verify_key_hex[i])))
+        new2.append(Output(100,verify_key_hex[i+3]))
+        inputs_from_gen_tx.append(new1)
+        outputs_from_gen_tx.append(new2)
 
-output_list = []
-# Generate contents for gen block. All 8 pksk pairs get 100 coins
-for i in range (0,8):
-    output_list.append(Output(100, verify_key_hex[i]))
+    for i in range(0, 3):
+        message = serialize_list(inputs_from_gen_tx[i], "input")
+        message += serialize_list(outputs_from_gen_tx[i], "output")
+        message = message.encode("utf-8")
+        sig.append(signing_key[i].sign(message))
 
+    
+    for i in range(0, 3):
+        global_tx_pool.append(Transaction(inputs_from_gen_tx[i], outputs_from_gen_tx[i], sig[i]))
+    
 
-empty_input_list = []
-gen_transaction = Transaction(empty_input_list, output_list, 0)
-
-j = gen_transaction.jsonify()
-
-
-
-# generate the number for the transaction
-gen_transaction.gen_number()
-gen_transaction_number = gen_transaction.number
-
-# Generate genesis block
-gen_block = Block(gen_transaction, b'0', None, b'0')
-
-#Initialize nodes with genesis block
-node_list = []
-for i in range (0,10):
-    node_list.append(Node(gen_block))
-
-for i in range (0,10):
-    node_list[i-1].node_list = node_list
-
-global_tx_pool = []
-no_more_tx = 1
-lock = 0
-inputs_from_gen_tx = []
-outputs_from_gen_tx = []
-
-for i in range (0,3):
-    inputs_from_gen_tx.append(Input(gen_transaction_number, Output(100, verify_key_hex[i])))
-    outputs_from_gen_tx.append(Output(100, verify_key_hex[i+3]))
-
-message = serialize_list(inputs_from_gen_tx, "input")
-message += serialize_list(outputs_from_gen_tx, "output")
-message = message.encode("utf-8")
-sig = signing_key[i].sign(message)
-for x in range (0,3):
-    global_tx_pool.append(Transaction(inputs_from_gen_tx[i], outputs_from_gen_tx[i], sig))
-
-print(type(node_list[1]))
-print(type(global_tx_pool[1]))
-node_list[1].mining
-print(node_list[1].current_max_height_tree_node.height)
+    print("======mine node[1]=========")
+    node_list[1].mining(global_tx_pool)
+    print(node_list[1].current_max_height_tree_node.height)
 
 
 
